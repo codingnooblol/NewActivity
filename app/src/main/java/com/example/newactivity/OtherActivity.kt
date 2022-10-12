703d55d802055636464a2a840aca1c33b8b23e7e
Kim <xzsa18@naver.com>
2022-10-12 20:12:31 +0900

    finish() 이용 -> 현재화면 종료, 이전화면 복귀

--- app/src/main/java/com/example/newactivity/OtherActivity.kt
+++ app/src/main/java/com/example/newactivity/OtherActivity.kt
@@ -1,11 +1,22 @@
 package com.example.newactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainActivity.setOnClickListener {
+//            MainActivity로 돌아가는 코드
+
+//            val returnIntent = Intent(this, MainActivity::class.java)
+//            startActivity(returnIntent)
+
+            finish()
+        }
     }
 }
