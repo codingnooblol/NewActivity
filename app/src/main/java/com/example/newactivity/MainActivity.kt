36e0e3091e7f22c6d7f08d6f7a449b2bbed9d1b1
Kim <xzsa18@naver.com>
2022-10-13 14:55:08 +0900

    View 액션 예제 - 인터넷 링크 - 플레이 스토어 상세화면 링크

--- app/src/main/java/com/example/newactivity/MainActivity.kt
+++ app/src/main/java/com/example/newactivity/MainActivity.kt
@@ -27,6 +27,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        naverWebBtn.setOnClickListener {
+
+            val myUri = Uri.parse("https://www.naver.com")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
+
+        kakaoStoreBtn.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
+
 
 //        DIAL 액션 예제
 
